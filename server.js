6af3f142c7ae8aa8ea2b2379dad0647c338b8003
root <root@ip-172-31-14-66.eu-west-2.compute.internal>
2017-09-04 16:08:33 +0000

    Added some files

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var users = require('./routes/users');
 var cors = require('cors')
 var app = express();
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'dist'));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(logger('dev'));
@@ -25,7 +25,7 @@ app.use('/', index);
 app.use('/api/v1/', todos);
 app.use('/api/v1/', pages);
 app.use('/api/v1/', users);
-app.use(express.static('views'));
+app.use(express.static('dist'));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
 
