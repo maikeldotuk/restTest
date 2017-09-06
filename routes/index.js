61097707e4d438fa2c65ed4f079dda64432f8ec9
maike <maikel@maikel.uk>
2017-09-07 00:46:05 +0100

    Shite

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var FroalaEditor = require('./../node_modules/wysiwyg-editor-node-sdk/lib/froalaEditor.js');
+
+var bodyParser = require('body-parser')
+var path = require('path');
+var fs = require('fs');
+
+app.use(express.static(__dirname + '/'));
+app.use('/bower_components',  express.static(path.join(__dirname, '../bower_components')));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index.html');
