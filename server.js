5bef1e1ee06dfa4a14e4cf9dddbc2e8c22576071
root <root@ip-172-31-14-66.eu-west-2.compute.internal>
2017-09-07 11:49:11 +0000

    Changed folder of the CMS so I could update it using git

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ var todos = require('./routes/todos');
 var pages = require('./routes/pages');
 var users = require('./routes/users');
 var cors = require('cors')
+var serveIndex = require('serve-index');
 
 var app = express();
 app.enable('trust proxy');
 // view engine setup
-app.set('views', path.join(__dirname, 'dist'));
+app.set('views', path.join(__dirname, 'cmsDIST'));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(logger(':method :url :status - :res[content-length] :user-agent :remote-addr'));
@@ -21,14 +22,14 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use('/', index);
 app.use('/api/v1/', todos);
 app.use('/api/v1/', pages);
 app.use('/api/v1/', users);
-app.use(express.static('dist'));
+app.use(express.static('cmsDIST'));
 app.use('/images', express.static('images'));
+app.use('/images', serveIndex('public/images', {'icons': true}));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
 
